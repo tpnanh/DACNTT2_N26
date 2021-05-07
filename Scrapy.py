aebbd9eeb4c6116f548afe221fa02f3be3a27a96
Anise <anh.tran@staff.gumiviet.com>
2021-05-08 00:21:31 +0700

    Week 3: crawl articles

--- Scrapy.py
+++ Scrapy.py
@@ -1,33 +1,114 @@
 import pyodbc
 import scrapy
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
+import requests
+from lxml import html 
+import datetime
 
-class MySpider(scrapy.Spider):
+class MySpider(scrapy.Spider):    
+    name = "crawl_news"         
     
-    name = "crawl_news" 
-        
+    def connectDB(self):    
+        conn = pyodbc.connect('Driver={SQL Server};'
+                          'Server=ANISE-TR\SQLEXPRESS;'
+                          'Database=WebDB;'
+                          'Trusted_Connection=yes;')    
+        cursor = conn.cursor()
+        return cursor
     
-    # conn = pyodbc.connect('Driver={SQL Server};'
-    #                   'Server=ANISE-TR\SQLEXPRESS;'
-    #                   'Database=WebDB;'
-    #                   'Trusted_Connection=yes;')
-
-    # cursor = conn.cursor()
-    # cursor.execute('SELECT * FROM NEWSINFO')
-
-    # start_urls = ['http://www.moit.gov.vn/web/guest/thoi-su']
-    start_urls = ['http://www.moit.gov.vn/web/guest/tin-chi-tiet/-/chi-tiet/co-hoi-quang-ba-hang-viet-nam-xuat-khau-vao-thi-truong-a-rap-xe-ut-khoi-thi-truong-a-rap-22024-22.html']
-
-    def parse(self, response): 
-        url = '//*[@id="contentnews"]/p/text()'
-        stri = response.xpath(url).extract()
-        print(stri)
-        print("Size: "+str(len(stri)))
-        # for (i in range(stri.size)):
+    
+    def covertToResponse(self,url):
+        response = requests.get(url)            
+        doc = html.fromstring(response.text)
+        return doc
+    
+    
+    def getUrl(self):
+        category_page = self.connectDB().execute('select category_link_root from category_info')
+        for row in category_page:   
+            print("Here: "+str(row[0]))
+            self.parseCategoryResponse(self.covertToResponse(row[0]))
+    
+    
+    def parseCategoryResponse(self, response): 
+        category_detail = self.connectDB().execute('select article_url_xpath,article_thumbnail_xpath from ministry_category_configuration')
+        for row in category_detail:
+            for i in range (len(row)):
+                # print("Content: "+str(row[i]))
+                ## i = 0 for article url to save article to DB
+                if (i == 0):                    
+                    article_url_xpath = response.xpath(row[i])
+                    for url_index in range (len(article_url_xpath)):
+                        # print("Url: "+str(article_url_xpath[url_index]))
+                        self.parseArticleResponse(article_url_xpath[url_index])
+                ## i = 1 for article thumbnail       
+                else:
+                    article_thumbnail_xpath = response.xpath(row[i])
+                    print("Thumb: "+str(article_thumbnail_xpath))
+                        
+                        
+    def parseArticleResponse(self, article_url): 
+        article_response = self.covertToResponse(article_url)
+        article_detail = self.connectDB().execute('select article_title_xpath,article_description_xpath,article_time_xpath,article_author_xpath,article_content_xpath from ministry_articles_configuration')
+        for row in article_detail:
+            article_title = ""
+            article_description = ""
+            article_time = ""
+            article_author_xpath = ""
+            article_content = ""
+            for i in range (len(row)):
+                if (i == 0):
+                    article_title = article_response.xpath(row[i])[0]
+                    print("Title: "+article_title)
+                elif (i == 1):
+                    article_description = article_response.xpath(row[i])[1]
+                    print("Des: "+article_description)
+                elif (i == 2):
+                    article_time = article_response.xpath(row[i])[0]
+                    print("Time: "+article_time)
+                elif (i == 3):
+                    article_author_xpath =  article_response.xpath(row[i])
+                    print("Author: "+str(article_author_xpath))
+                elif (i == 4):
+                    article_content = self.clearSpace(article_response.xpath('//*[@id="contentnews"]//text()'))
+                    print("Content: "+str(article_content))
+            print("\n")
             
-                  # .extract()[i].strip())
+            
+    # def parseCategoryResponse(self, article_thumbnail): 
+    #     article_response = self.covertToResponse(article_thumbnail)
+                    
+            
+    def clearSpace(self, listString):
+        return [string for string in listString if string != ' ']
+                
+                    
+    def covertStringFromArticleToSqlFormat (self, dateString):
+        #from 28/02/2021 to 2021/02/28
+        dt = datetime.datetime.strptime(dateString, '%d/%m/%Y')
+        return '{2}/{1:02}/{0:02}'.format(dt.day, dt.month, dt.year)
+        
 
-c = CrawlerProcess(get_project_settings())
-c.crawl(MySpider)
-c.start()
+    def covertStringFromSqlToArticleFormat (self, dateString):
+        #from 2021/02/28 to 28/02/2021
+        dt = datetime.datetime.strptime(dateString, '%Y/%m/%d')
+        return '{0:02}/{1:02}/{2}'.format(dt.day, dt.month, dt.year)
+    
+    
+    def saveArticleToDB(self):
+        conn = pyodbc.connect('Driver={SQL Server};'
+                          'Server=ANISE-TR\SQLEXPRESS;'
+                          'Database=WebDB;'
+                          'Trusted_Connection=yes;')    
+        cursor = conn.cursor()        
+        cursor.execute('''
+                INSERT INTO WebDB.dbo.article_info (Name, Age, City)
+                VALUES
+                ('Bob',55,'Montreal'),
+                ('Jenny',66,'Boston')
+                ''')
+        conn.commit()
+        
+        
+
+p = MySpider()
+p.getUrl()
